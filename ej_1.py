20d80d16a74989c74a11bc826f1c3b6905d76daf
Mercedes <mercetetilla@gmail.com>
2023-08-15 13:45:24 -0300

    Primer commit

---
+++ ej_1.py
@@ -0,0 +1,8 @@
+n1 = 3
+n2 = 4
+may = 0
+if (n1 > n2):
+   may = n1;
+if n2 > may:
+   may == n2;
+print(may)
---
+++ ej_comp.py
@@ -0,0 +1,65 @@
+#Ejercicio 1
+numero1 = 5
+
+#Ejercicio 2
+numero2 = 8.5
+
+#Ejercicio 3
+suma = (numero1 + numero2)
+
+#Ejercicio 4
+rest = (numero1 - numero2)
+multi = (numero1 * numero2)
+divi = (numero1 / numero2)
+print(f"suma: {suma}, resta: {rest}, multi: {multi}, divi: {divi}")
+
+#Ejercicio 5
+nombre = "Mercedes"
+
+#Ejercicio 6
+precio = 199.9
+
+#Ejercicio 7
+descuento = 0.10
+
+#Ejercicio 8
+precio_final = (precio * descuento)
+print(f"precio final: {precio_final}")
+
+#Ejercicio 9
+cadena = "Hola mundo"
+
+#Ejercicio 10
+longitud = len(cadena)
+print(f"longitud: {longitud}")
+
+#Ejercicio 11
+precio = int(100.5)
+print(f"precio {precio}")
+
+#Ejercicio 12
+nombre = "Mercedes"
+apellido = " Tetilla"
+nombre_completo = {nombre + apellido}
+print(nombre_completo)
+
+#Ejercicio 13
+edad = 18
+edad += 5
+edad -= 10
+print(f"{edad}")
+
+#Ejercicio 14
+altura = (1.69 * 4) /3
+print(f"{altura}")
+
+#Ejercicio 15
+nombre = "MERCEDES"
+minusculas = nombre.lower()
+print(f"{minusculas}")
+
+#Ejercicio 16
+mayus = nombre[0]
+resto = nombre [1, 2, 3, 4, 5, 6, 7]
+minus = resto.lower()
+print(mayus + minus)
