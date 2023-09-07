33e20f47623c7810efdcb23eacc3b27502a9ed6f
Mercedes <mercetetilla@gmail.com>
2023-09-07 11:07:02 -0300

    condicionales corregido

--- condicionales.py
+++ condicionales.py
@@ -5,10 +5,8 @@ dd = int(fecha[fecha.find(" ")+1: fecha.find("/")])
 mm = int(fecha[fecha.find("/")+1:])
 
 print(f"{dia}, {dd}/{mm}")
-if ((dia != "lunes" and dia != "martes" and dia != "miercoles" and dia != "jueves" and dia != "viernes") or (mm > 12 and mm<1) or (dd<1 and dd>31 )):
-    print(f"ERROR")
 
-if (dia == "lunes" or dia == "martes" or dia == "miercoles"):
+if ((dia == "lunes" or dia == "martes" or dia == "miercoles")  and (mm < 12 and mm>1) and (dd>1 and dd<31 )):
     ex = input("Hoy tomaron examenes? s/n")
     ex = ex.lower()
     if(ex == "s"):
@@ -17,22 +15,20 @@ if (dia == "lunes" or dia == "martes" or dia == "miercoles"):
         sum = ap+des
         por = (ap / sum)*100
         print(f"El porsentaje de aprobados es: {por}")
-        
+elif ((dia == "jueves") and (mm < 12 and mm>1) and (dd>1 and dd<31 )):
+    tot = int(input("Cual es el total de los alumnos?"))
+    asi = int(input("Cuantos alumnos asistieron a clases?"))
+    por = ((asi * 100)/tot)
+    if (por > 50):
+        print("Asistio la mayoria")
+    else:
+        print("No asistio la mayoria")
+elif ((dia == "viernes") and (mm < 12 and mm>1) and (dd>1 and dd<31 )):
+    if((mm == 1 and dd == 1)or(mm==7 and dd==1)):
+        print("Comienzo del nuevo ciclo")
+        nuevo = int(input("Ingrese la cantidad de alumnos nuevos:"))
+        ara = float(input("Ingrese el arancel por alumno"))
+        tot = nuevo*ara
+        print(f"Ingrso total: {tot}")
 else:
-    if(dia == "jueves"):
-        tot = int(input("Cual es el total de los alumnos?"))
-        asi = int(input("Cuantos alumnos asistieron a clases?"))
-        por = ((asi * 100)/tot)
-        if (por > 50):
-            print("Asistio la mayoria")
-        else:
-            print("No asistio la mayoria")
-    else: 
-        if(dia == "viernes"):
-            if(dd == 1 and mm == 1 or dd == 1 and mm == 7):
-                print("Comienzo del nuevo ciclo")
-                nuevo = int(input("Ingrese la cantidad de alumnos nuevos:"))
-                ara = float(input("Ingrese el arancel por alumno"))
-                tot = nuevo*ara
-                print(f"Ingrso total: {tot}")
-
+    print("ERROR")
