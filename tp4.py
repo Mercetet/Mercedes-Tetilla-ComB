10aa650b7b76f5104bca6aca384e78715ec7c9ff
Mercedes <mercetetilla@gmail.com>
2023-09-10 16:43:57 -0300

    TP 4 completo.

--- tp4.py
+++ tp4.py
@@ -1,3 +1,26 @@
+# 1. Create a while loop with the following characteristics:
+
+# • The initial value of the variable x will be 0.
+# • The increment value will be 1.
+# • The exit condition of the loop will be greater than or equal to 30.
+# • The execution must be broken when x is equal to 15.
+# • You must print the following sentence each time the loop executes: 'The value of the loop is: ' + x.
+# • You must skip the executions with the value of x in 4, 6 and 10.
+# • At each execution jump, you must display the jumped values with this message: 'The value ' + x ' of x was skipped'.
+# • When the execution of the loop is broken, you must show a message indicating it: 'The execution of the loop was broken when x was ' + x.
+x = 0
+
+while x < 30:
+    x += 1
+    if x == 15:
+        break
+    elif (x == 4) or (x == 6) or (x == 10):
+        print(f"The value {x} of x was skipped")
+    else:
+        print(f"The value of the loop is: {x}")
+
+print(f"The execution of the loop was broken when x was {x}")
+
 # 1.	Escriba un programa que acepte una secuencia de líneas e imprima todas las líneas convertidas en mayúsculas. Deje una línea en blanco para indicar que ha finalizado la entrada de líneas.
 
 sentence = str(input("Ingrese una frase: "))
